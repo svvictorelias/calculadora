fb7d5ab03866d3c73df6104b78719c1a099e9fea
Victor Elias <svvictorelias@gmail.com>
2022-04-21 19:41:33 -0300

    criado calculadora para consolidar conhecimentos em arrow functions e classes construtoras

---
+++ assets/js/main.js
@@ -0,0 +1,52 @@
+function Calculadora(){
+    this.display = document.querySelector('.display')
+    this.inicia = () =>{
+        this.capturaClique()
+        this.capturaEnter()
+    }
+
+    this.capturaClique = () =>{
+        document.addEventListener('click',e =>{
+            const el = e.target
+            if(el.classList.contains('btn-num')) this.addNumber(el)
+            if(el.classList.contains('btn-eq')) this.realizaConta()
+            if(el.classList.contains('btn-del')) this.delNumber()
+            if(el.classList.contains('btn-clean')) this.cleanNumber()
+        })
+    }
+    this.capturaEnter = () =>{
+        document.addEventListener('keypress',e =>{
+            const el = e.target
+            if(e.keyCode !==13)return
+            this.realizaConta()
+        })
+    }
+
+    this.addNumber = (el) =>{
+        this.display.value += el.innerText
+        this.display.focus()
+    }
+    
+    this.delNumber = () => this.display.value = this.display.value.slice(0,-1)
+
+    this.cleanNumber = () => this.display.value = ''
+
+    this.realizaConta = () =>{
+        try{
+            let conta = this.display.value.replace(',','.')
+            conta = eval(conta)
+            if(!conta){
+                alert('Conta Invalida')
+                return
+            }
+            this.display.value = conta
+        }catch(e){
+            alert('conta invalida')
+            return
+        }
+    }
+
+}
+
+calculadora = new Calculadora()
+calculadora.inicia()
